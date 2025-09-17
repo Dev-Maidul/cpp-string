7aedf4ac3d2f28e85de17d3c9b50e85daffe2088
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-09-17 12:38:41 +0800

    frequency count

--- Map/Frequency.cpp
+++ Map/Frequency.cpp
@@ -1,4 +1,6 @@
 
+// https://www.geeksforgeeks.org/problems/find-the-frequency/1
+// Map store the unique value with key and pairs , normally used ot count the frequency of a number. 
 class Solution {
   public:
     int findFrequency(vector<int> arr, int x) {
