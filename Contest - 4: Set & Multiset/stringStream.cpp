e5ea6517607ae05db391786e1e0ce484f64da413
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-09-24 02:16:53 +0800

    Learn stringStream

---
+++ Contest - 4: Set & Multiset/stringStream.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    string sentence;
+    set<string>st;
+    while(getline(cin,sentence))
+    {
+        if(sentence=="#") break;
+        stringstream ss(sentence);
+        string word;
+        while(ss>>word)
+        {
+            st.insert(word);
+        }
+        
+    }
+    cout<<st.size()<<endl;
+}
