6305c5884feee6b480e59813221a51ad058c2855
Dev-Maidu <maidulislammanik8991@gmail.com>
2025-09-29 00:35:13 +0800

    Recap Pair in STL

---
+++ Pair/Pair.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	pair<int,string> student;
+	vector<pair<int,string>>allStudents;
+	int n;
+	cin>>n;
+	int roll;
+	string name;
+	for(int i=0; i<n; i++)
+	{
+	    cin>>roll>>name;
+	    student=make_pair(roll,name);
+	    allStudents.push_back(student);
+	}
+	for(auto curStudent: allStudents)
+	{
+	    cout<<"Roll: "<<curStudent.first<<" Name: "<<curStudent.second<<endl;
+	}
+
+}
